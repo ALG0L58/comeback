1d0d45d0794402e08df9dd86451104561d950284
_BLacK_JacK_ <iiggy2661@gmail.com>
2025-02-05 17:22:37 +0200

    add default todo and styles for them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './styles/App.css';
+
+const DEFAULT_TODO_LIST = [
+  {id: 1, name: 'task1', desicriptions: 'desicriptions1', checked: false},
+  {id: 2, name: 'task2', desicriptions: 'desicriptions2', checked: false},
+  {id: 3, name: 'task3', desicriptions: 'desicriptions3', checked: true}
+]
+
+
 
 function App() {
+  
+
   return (
-    <div className="App">
-      
+    <div className='app_container'>
+      <div className="container">todos</div>
     </div>
   );
 }
